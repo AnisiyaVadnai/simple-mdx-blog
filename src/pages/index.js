af0ce37e97ead137319848826af90306d15aad47
Anisiya Vadnai <av@vaiver.ru>
2022-05-21 21:54:47 +0400

    Added card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 
 const IndexPage = () => {
   return(
     <Container>
          <FeatureImage />
+         <Content>
+           <ContentCard 
+            date="20 July 2022" 
+            title="Excepteur sint obcaecat cupiditat"
+            excerpt=" Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam," 
+            slug="/test" />
+         </Content>
     </Container>
   )
 }
